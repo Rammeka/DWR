d29fbee48d4640b6a4fd73bf0c3d0c854af9c833
Ram <ram.meka@ezcorp.com>
2013-09-23 13:44:08 -0500

    Test

---
+++ SampleNew/src/com/samplenew/test/test.java
@@ -0,0 +1,8 @@
+package com.samplenew.test;
+
+public class test {
+	public String getMessage()
+	{
+		return "Hello DWR app";
+	}
+}
---
+++ dwr/WebContent/widgets/dojo/custom/jsonrpcDemo.js
@@ -0,0 +1,8 @@
+/*
+	Copyright (c) 2004-2008, The Dojo Foundation All Rights Reserved.
+	Available via Academic Free License >= 2.1 OR the modified BSD license.
+	see: http://dojotoolkit.org/license for details
+*/
+
+
+if(!dojo._hasResource["custom.jsonrpcDemo"]){dojo._hasResource["custom.jsonrpcDemo"]=true;dojo.provide("custom.jsonrpcDemo");dojo.require("dojox.rpc.Service");dojo.require("dojox.rpc.JsonRPC");}
